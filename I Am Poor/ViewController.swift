4e02e9eac1a239f307952a41ebee13eac1133af6
Matheus Paes Crescêncio <matheuspaes700@gmail.com>
2021-05-24 01:37:50 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Matheus Paes Crescêncio on 24/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
